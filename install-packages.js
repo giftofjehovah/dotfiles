e767cd5fa48a8cb4ed11188ce2ad5b718758575c
Jonathan Phoon <jonathan@aleph-labs.com>
2016-12-24 22:44:33 +0800

    fix path error on install package

--- install-packages.js
+++ install-packages.js
@@ -6,7 +6,7 @@ const installPackages = function (type) {
   console.info(emoji.get('coffee'), ' installing ' + type + ' packages')
   config[type].map(function (item) {
     console.info(type + ':', item)
-    command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_' + type + ' ' + item, __dirname, function (err, out) {
+    command('. helpers/echos.sh && . helpers/requirers.sh && require_' + type + ' ' + item, __dirname, function (err, out) {
       if (err) console.error(emoji.get('fire'), err)
     })
   })
