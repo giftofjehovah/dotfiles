dabc0187e211585c94c410637ba9b43c4f6531ae
Jonathan Phoon <jonathan@aleph-labs.com>
2016-12-24 23:17:55 +0800

    update path for theme download

--- settings/macos.sh
+++ settings/macos.sh
@@ -295,7 +295,7 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true;ok
 
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
 	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-	"Opera" "Photos" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
+	"Opera" "Photos" "Safari" "SizeUp" "Spectacle" "SystemUIServer" \
 	"Transmission" "Tweetbot" "Twitter" "iCal"; do
 	killall "${app}" &> /dev/null
 done
