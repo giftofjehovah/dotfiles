2a0b6f5eaee42637f4e37b210127cbcd4c8a25b2
Jonathan Phoon <jonathan@aleph-labs.com>
2016-12-24 23:26:25 +0800

    update helper path in macos

--- settings/macos.sh
+++ settings/macos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ~/.macos — https://mths.be/macos
-source ../helpers/echos.sh
+source ./helpers/echos.sh
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
