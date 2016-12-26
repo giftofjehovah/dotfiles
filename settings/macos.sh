df16ffb41d96a8a8038cb8a5d4485602bca0bd70
Jonathan Phoon <jonathan@aleph-labs.com>
2016-12-26 15:50:25 +0800

    add command for dark mode in macos

--- settings/macos.sh
+++ settings/macos.sh
@@ -232,6 +232,9 @@ running "Add iOS & Watch Simulator to Launchpad"
 sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
 sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (Watch).app" "/Applications/Simulator (Watch).app";ok
 
+running "Change macos interface to dark"
+defaults write NSGlobalDomain AppleInterfaceStyle Dark;ok
+
 ###############################################################################
 bot "Safari & WebKit"                                                         #
 ###############################################################################
