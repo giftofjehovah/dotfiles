c12764ee86f974fa6f5819320f504b8e551ec1db
Jonathan Phoon <jon@Jonathans-MacBook-Pro.local>
2017-03-09 21:37:01 +0800

    add pages, numbers, keynote

--- install/mas.js
+++ install/mas.js
@@ -2,7 +2,10 @@ const mas = [
   '918858936', // airmail
   '1051767777', // meistertask
   '732710998', // enpass
-  '497799835' // xcode
+  '497799835', // xcode
+  '409183694', // keynote
+  '409203825', // numbers
+  '409201541' // pages
 ]
 
 module.exports = mas
