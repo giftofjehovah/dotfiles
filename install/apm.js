492edf1ca416abd09519153e6c5cf869b23db5ae
Jonathan Phoon <jon@Jonathans-MacBook-Pro-2.local>
2017-07-16 20:33:23 +0800

    add proto-repl to apm

--- install/apm.js
+++ install/apm.js
@@ -1,24 +1,25 @@
 const apm = [
-  'advanced-open-file',
-  'atom-beautify',
-  'atom-typescript',
-  'emmet',
-  'file-icons',
-  'autoclose-html',
-  'highlight-selected',
-  'language-babel',
-  'language-ejs',
-  'linter',
-  'linter-js-standard',
-  'minimap',
-  'pigments',
-  'set-syntax',
-  'standard-formatter',
-  'standardjs-snippets',
-  'vim-mode',
-  'react',
-  'parinfer',
-  'atom-panda-syntax'
-]
+  "advanced-open-file",
+  "atom-beautify",
+  "atom-typescript",
+  "emmet",
+  "file-icons",
+  "autoclose-html",
+  "highlight-selected",
+  "language-babel",
+  "language-ejs",
+  "linter",
+  "linter-js-standard",
+  "minimap",
+  "pigments",
+  "set-syntax",
+  "standard-formatter",
+  "standardjs-snippets",
+  "vim-mode",
+  "react",
+  "parinfer",
+  "atom-panda-syntax",
+  "proto-repl"
+];
 
-module.exports = apm
+module.exports = apm;
