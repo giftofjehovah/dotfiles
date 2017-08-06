01e8fac90363905c0ac25c355ad4a637e1887bf0
Jonathan Phoon <jon@Jonathans-MacBook-Pro-2.local>
2017-08-07 00:02:51 +0800

    add rescuetime to cask

--- install/brew-cask.js
+++ install/brew-cask.js
@@ -1,67 +1,68 @@
 const brewCask = [
-  "atom",
-  "evernote",
-  "iterm2",
-  "slack",
-  "google-chrome",
-  "firefox",
-  "alfred",
-  "spectacle",
-  "bartender",
-  "airserver",
-  "franz",
-  "logic",
-  "emacs",
-  "zeplin",
-  "flux",
-  "ubersicht",
-  "sourcetree",
-  "intellij-idea",
-  "postman",
-  "sketch",
-  "razer-synapse",
-  "myo-connect",
-  "steam",
-  "sequel-pro",
-  "postgres",
-  "virtualbox",
-  "hyper",
-  "cargo",
-  "keybase",
-  "mongodb-compass",
-  "visual-studio-code",
-  "now",
-  "duet",
-  "exodus"
-];
+  'atom',
+  'evernote',
+  'iterm2',
+  'slack',
+  'google-chrome',
+  'firefox',
+  'alfred',
+  'spectacle',
+  'bartender',
+  'airserver',
+  'franz',
+  'logic',
+  'emacs',
+  'zeplin',
+  'flux',
+  'ubersicht',
+  'sourcetree',
+  'intellij-idea',
+  'postman',
+  'sketch',
+  'razer-synapse',
+  'myo-connect',
+  'steam',
+  'sequel-pro',
+  'postgres',
+  'virtualbox',
+  'hyper',
+  'cargo',
+  'keybase',
+  'mongodb-compass',
+  'visual-studio-code',
+  'now',
+  'duet',
+  'exodus',
+  'rescuetime'
+]
 
 // Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 const quickLookPlugin = [
-  "qlcolorcode",
-  "qlstephen",
-  "qlmarkdown",
-  "quicklook-json",
-  "qlprettypatch",
-  "quicklook-csv",
-  "betterzipql",
-  "qlimagesize",
-  "webpquicklook",
-  "suspicious-package"
-];
+  'qlcolorcode',
+  'qlstephen',
+  'qlmarkdown',
+  'quicklook-json',
+  'qlprettypatch',
+  'quicklook-csv',
+  'betterzipql',
+  'qlimagesize',
+  'webpquicklook',
+  'suspicious-package'
+]
 
 const fonts = [
-  "font-awesome-terminal-fonts",
-  "font-hack",
-  "font-inconsolata-dz-for-powerline",
-  "font-inconsolata-g-for-powerline",
-  "font-inconsolata-for-powerline",
-  "font-roboto-mono",
-  "font-roboto-mono-for-powerline",
-  "font-source-code-pro",
-  "font-meslo-lg-for-powerline",
-  "font-firacode-nerd-font-mono"
-];
+  'font-awesome-terminal-fonts',
+  'font-hack',
+  'font-inconsolata-dz-for-powerline',
+  'font-inconsolata-g-for-powerline',
+  'font-inconsolata-for-powerline',
+  'font-roboto-mono',
+  'font-roboto-mono-for-powerline',
+  'font-source-code-pro',
+  'font-meslo-lg-for-powerline',
+  'font-firacode-nerd-font-mono'
+]
 
-const all = [...brewCask, ...quickLookPlugin, ...fonts];
+const all = [...brewCask, ...quickLookPlugin, ...fonts]
 
-module.exports = all;
+module.exports = all
