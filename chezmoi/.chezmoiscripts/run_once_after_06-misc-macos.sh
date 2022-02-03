87d1e721d4c18de84d339590fc9a60dde92227f5
Jonathan Phoon <jonathan@stashaway.com>
2022-02-03 11:01:38 +0800

    ADD last few commands

--- chezmoi/.chezmoiscripts/run_once_after_06-misc-macos.sh
+++ chezmoi/.chezmoiscripts/run_once_after_06-misc-macos.sh
@@ -1,3 +1,11 @@
 sudo xcodebuild -license accept
+
 mackup restore
+
 mkdir ~/dev
+
+pub=$HOME/.ssh/id_rsa.pub
+echo 'Checking for SSH key, generating one if it does not exist...'
+[[ -f $pub ]] || ssh-keygen -t rsa
+
+brew cleanup
