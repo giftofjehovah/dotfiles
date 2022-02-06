bfebdf3b7a7ea556533c7717595c2cbff8bdd55a
Jonathan Phoon <jonathan@stashaway.com>
2022-02-06 16:29:42 +0800

    ADD git-open

--- dot_aliases.sh
+++ dot_aliases.sh
@@ -1,7 +1,7 @@
 alias dl="cd ~/Downloads"
 alias dt="cd ~/Desktop"
 alias dev="cd ~/dev"
-alias dotf="cd ~/.dot"
+alias dotf="cd ~/.local/share/chezmoi"
 
 # Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
@@ -18,11 +18,11 @@ alias -g C='| xargs echo -n | pbcopy'
 
 # Copy my public key to the pasteboard
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'"
-alias wifi="wifi-password"
+# alias wifi="wifi-password"
 
 # swap python env
 # alias pip3="source activate python3"
-alias pip2="source deactivate"
+# alias pip2="source deactivate"
 
 # gcloud commands
 alias gstart="gcloud compute instances start"
